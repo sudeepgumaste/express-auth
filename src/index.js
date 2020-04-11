7f14b7d043bbab4f5974876e3aa786e808086d7e
Sudeep Gumaste <sudeepgumaste19@gmail.com>
2020-04-11 11:16:13 +0530

    Added morgan logger

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import process from 'process';
 import { router } from './router';
 import mongoose from 'mongoose';
 import cors from 'cors'
+import morgan from 'morgan';
+import fs from 'fs';
 
 //configuring environment
 dotenv.config();
@@ -12,6 +14,8 @@ dotenv.config();
 const app = express();
 
 app.use(cors())
+app.use(morgan('dev'))
+app.use(morgan('combined', {stream: fs.createWriteStream('./logs/requests.log', {flags: 'a'})}))
 
 //db connection
 mongoose.connect(
